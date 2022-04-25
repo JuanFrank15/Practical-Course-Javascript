49682530616b4666e676d7f8b4908a1c42d2382f
Juan F. Martines <juanfrank.m15@gmail.com>
2022-04-25 17:11:22 -0500

    Se introdujo un Swicth para manejar los descuento con un array

--- descuento.js
+++ descuento.js
@@ -7,18 +7,36 @@ function calcularPrecioConDescuento(precio, descuento) {
   return precioConDescuento;
 }
 
+const coupons = [
+    "JuanDC es Batman",
+    "pero no le digas a nadie",
+    "es un secreto",
+];
+
 function onClickButtonPriceDiscount() {
-  const inputPrice = document.getElementById("InputPrice");
-  const priceValue = inputPrice.value;
-
-  const inputDiscount = document.getElementById("InputDiscount");
-  const discountValue = inputDiscount.value;
-
-  const precioConDescuento = calcularPrecioConDescuento(
-    priceValue,
-    discountValue
-  );
-
-  const resultP = document.getElementById("ResultP");
-  resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
-}
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+  
+    let descuento;
+  
+    switch(couponValue) {
+      case coupons[0]: // "JuanDC_es_Batman"
+        descuento = 15;
+      break;
+      case coupons[1]: // "pero_no_le_digas_a_nadie"
+        descuento = 30;
+      break;
+      case coupons[2]: // "es_un_secreto"
+        descuento = 25;
+      break;
+    }
+  
+  
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+  
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+  }
